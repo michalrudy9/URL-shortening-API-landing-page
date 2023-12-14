9ede75ecac2bc59ee7a43891dc596b7c950ac58c
michalrudy9 <michallrudy9@gmail.com>
2023-12-14 11:15:59 +0100

    Added page skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,30 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { NavigationComponent } from './navigation/navigation.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { ShortenUrlComponent } from './shorten-url/shorten-url.component';
+import { ShortenedLinksComponent } from './shortened-links/shortened-links.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { FooterComponent } from './footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavigationComponent,
+    JumbotronComponent,
+    ShortenUrlComponent,
+    ShortenedLinksComponent,
+    StatisticsComponent,
+    GetStartedComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'URL-shortening-API-landing-page';
---
+++ src/app/shortened-links/shortened-links.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shortened-links',
+  standalone: true,
+  imports: [],
+  templateUrl: './shortened-links.component.html',
+  styleUrl: './shortened-links.component.scss'
+})
+export class ShortenedLinksComponent {
+
+}
