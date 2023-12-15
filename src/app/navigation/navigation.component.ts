b3f93adb9a987d0cf7ea67df591e2b5e6cd7bd12
michalrudy9 <michallrudy9@gmail.com>
2023-12-15 12:32:27 +0100

    Added page navigation styling

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,12 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrl: './navigation.component.scss',
+
 })
 export class NavigationComponent {
+  protected isMenuCollapsed: boolean = true;
 
+  protected onAnimateCollapse() {
+    this.isMenuCollapsed = !this.isMenuCollapsed
+  }
 }
