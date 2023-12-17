ffa7e05e4dae849a3a38403547a6f56d76e06838
michalrudy9 <michallrudy9@gmail.com>
2023-12-17 18:41:45 +0100

    Created form and added validation

--- src/app/shorten-url/shorten-url.component.ts
+++ src/app/shorten-url/shorten-url.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-shorten-url',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './shorten-url.component.html',
-  styleUrl: './shorten-url.component.scss'
+  styleUrl: './shorten-url.component.scss',
 })
-export class ShortenUrlComponent {
+export class ShortenUrlComponent implements OnInit {
+  protected form!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      url: this.formBuilder.control(null, Validators.required),
+    });
+  }
+
+  onSubmit() {
+    const userUrl = this.form.value['url'];
+  }
 }
