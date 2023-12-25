4210ee0a088b44af1d7e6850af0120a5048b79e4
michalrudy9 <michallrudy9@gmail.com>
2023-12-25 16:25:51 +0100

    Added some fixes

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,9 @@
-import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [NgClass],
+  imports: [],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
